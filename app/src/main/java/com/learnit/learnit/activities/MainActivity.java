cc828e927ff7f73c680d64e589c91945fb3f3091
Igor Bogoslavskyi <igor.bogoslavskyi@gmail.com>
2015-09-27 15:18:00 +0200

    on my way. Fixed some layout bugs. This doesn't crash anymore.

--- app/src/main/java/com/learnit/learnit/activities/MainActivity.java
+++ app/src/main/java/com/learnit/learnit/activities/MainActivity.java
@@ -9,29 +9,21 @@ package com.learnit.learnit.activities;
 import android.app.Activity;
 import android.app.FragmentManager;
 import android.content.Intent;
-import android.content.res.Configuration;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.design.widget.TabLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
-import android.view.animation.AccelerateInterpolator;
-import android.view.animation.DecelerateInterpolator;
 import android.view.inputmethod.InputMethodManager;
-import android.widget.AdapterView;
 import android.widget.ImageView;
-import android.widget.ListView;
-import android.widget.TextView;
 
 import com.learnit.learnit.R;
 import com.learnit.learnit.async_tasks.PopulateHelpDictTask;
 import com.learnit.learnit.fragments.TaskSchedulerFragment;
-import com.learnit.learnit.interfaces.IActionBarEvents;
 import com.learnit.learnit.interfaces.IAsyncTaskResultClient;
 import com.learnit.learnit.types.TabsPagerAdapter;
 import com.learnit.learnit.utils.Constants;
@@ -43,7 +35,7 @@ import butterknife.InjectView;
 
 public class MainActivity
         extends AppCompatActivity
-        implements IActionBarEvents, IAsyncTaskResultClient {
+        implements IAsyncTaskResultClient {
     @InjectView(R.id.toolbar)
     Toolbar mToolbar;
     @InjectView(R.id.tab_layout)
@@ -51,23 +43,12 @@ public class MainActivity
     @InjectView(R.id.pager)
     ViewPager mPager;
 
-    FragmentPagerAdapter mPagerAdapter;
-//    @InjectView(R.id.drawer_layout)
-//    DrawerLayout mDrawerLayout;
-//    @InjectView(R.id.nav_drawer_list)
-//    ListView mDrawerListView;
-
-//    private int mOldScroll;
-//    private ActionBarDrawerToggle mDrawerToggle;
     private TaskSchedulerFragment mTaskScheduler;
-    private boolean mActionBarHidden;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        mActionBarHidden = false;
-//        mOldScroll = 0;
 
         if (Utils.isRunFirstTime(this.getLocalClassName())) {
             Log.d(Constants.LOG_TAG, "running " + this.getLocalClassName() + " for the first time");
@@ -75,10 +56,11 @@ public class MainActivity
             startActivity(new Intent(this, IntroActivity.class));
         }
         ButterKnife.inject(this);
-        mPager.setAdapter(new TabsPagerAdapter(getSupportFragmentManager(), this));
+        FragmentPagerAdapter mPagerAdapter = new TabsPagerAdapter(getSupportFragmentManager(), this);
+        mPager.setAdapter(mPagerAdapter);
+        mTabLayout.setTabsFromPagerAdapter(mPagerAdapter);
         mPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(mTabLayout));
         initActionBar();
-//        initSidePane();
         initTaskScheduler();
     }
 
@@ -89,92 +71,15 @@ public class MainActivity
         Utils.updateHelpDictIfNeeded(this, mTaskScheduler, this);
     }
 
-    @Override
-    protected void onPause() {
-        super.onPause();
-//        mDrawerLayout.closeDrawer(mDrawerListView);
-    }
-
     private void startSettingsActivity() {
         Intent intent = new Intent(this, SettingsActivity.class);
         startActivity(intent);
-    }
-
-//    @Override
-//    public void onScrollChanged(int newScroll, boolean b, boolean b2) {
-//        // The damping is needed to account for the implicit scroll that
-//        // occurs when the list view changes its size.
-//        if (newScroll - mOldScroll > mToolbar.getHeight()) {
-//            hideKeyboard(this);
-//            mOldScroll = newScroll;
-//            hideActionBar(mToolbar.getHeight());
-//        }
-//        if (newScroll - mOldScroll < -mToolbar.getHeight()) {
-//            hideKeyboard(this);
-//            mOldScroll = newScroll;
-//            showActionBar();
-//        }
-//    }
-//    @Override
-//    public void onDownMotionEvent() {}
-//    @Override
-//    public void onUpOrCancelMotionEvent(ScrollState scrollState) {}
-
-    @Override
-    protected void onPostCreate(Bundle savedInstanceState) {
-        super.onPostCreate(savedInstanceState);
-//        mDrawerToggle.syncState();
-    }
-
-    @Override
-    public void onConfigurationChanged(Configuration newConfig) {
-        super.onConfigurationChanged(newConfig);
-//        mDrawerToggle.onConfigurationChanged(newConfig);
-    }
-
-    @Override
-    public void showActionBar() {
-        if (!mActionBarHidden) {
-            // is shown already
-            return;
-        }
-        mToolbar.animate().translationY(0).setInterpolator(new DecelerateInterpolator(2));
-        mActionBarHidden = false;
-    }
-
-    @Override
-    public void hideActionBar(int actionBarHeight) {
-        if (mActionBarHidden) {
-            // already hidden
-            return;
-        }
-        mToolbar.animate().translationY(-actionBarHeight).setInterpolator(new AccelerateInterpolator(2));
-        mActionBarHidden = true;
     }
 
     private void initActionBar() {
         setSupportActionBar(mToolbar);
         adjustLogoSize();
     }
-
-//    private void initSidePane() {
-//        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout,
-//                mToolbar, R.string.str_learn_it, R.string.str_learn_it);
-//        mDrawerLayout.setDrawerListener(mDrawerToggle);
-//        mDrawerToggle.syncState();
-//
-//        mDrawerListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-//            @Override
-//            public void onItemClick(AdapterView<?> adapterView, View view, int clickedIndex, long l) {
-//                if (view instanceof TextView) {
-//                    if (clickedIndex == 0) {
-//                        startSettingsActivity();
-//                    }
-//                }
-//                Log.d(Constants.LOG_TAG, String.format("clicked view:%s, at pos:%s.", view.toString(), clickedIndex));
-//            }
-//        });
-//    }
 
     private static void hideKeyboard(Activity activity) {
         InputMethodManager inputMethodManager = (InputMethodManager) activity.getSystemService(Activity.INPUT_METHOD_SERVICE);
