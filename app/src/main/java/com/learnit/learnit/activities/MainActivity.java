549fd816e586bf67a5f5cd8651f2950721df1566
Igor Bogoslavskyi <igor.bogoslavskyi@gmail.com>
2015-10-08 23:41:25 +0200

    fixed tab behaviour

--- app/src/main/java/com/learnit/learnit/activities/MainActivity.java
+++ app/src/main/java/com/learnit/learnit/activities/MainActivity.java
@@ -60,6 +60,22 @@ public class MainActivity
         mPager.setAdapter(mPagerAdapter);
         mTabLayout.setTabsFromPagerAdapter(mPagerAdapter);
         mPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(mTabLayout));
+        mTabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                mPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
         initActionBar();
         initTaskScheduler();
     }
