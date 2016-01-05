d04500e5863be036807c3be0490f0d2957e48700
Igor Bogoslavskyi <igor.bogoslavskyi@gmail.com>
2016-01-05 18:30:16 +0100

    started adding gui for the learning fragment. Generates random words, assigns word to query word text view

---
+++ app/src/main/java/com/learnit/learnit/async_tasks/GetRandomUserWordsTask.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright (c) 2015.
+ * This code is written by Igor Bogoslavskyi. If you experience any issues with
+ * it please contact me via email: igor.bogoslavskyi@gmail.com
+ */
+
+package com.learnit.learnit.async_tasks;
+
+import android.content.Context;
+import android.util.Log;
+
+import com.learnit.learnit.db_handlers.DbHandler;
+import com.learnit.learnit.types.WordBundle;
+import com.learnit.learnit.utils.Constants;
+
+import java.util.List;
+
+public class GetRandomUserWordsTask extends MySmartAsyncTask<Integer, List<WordBundle>> {
+
+    protected String TAG = "get_random_words_task";
+
+    public GetRandomUserWordsTask(Context context,
+                                  Integer numberOfWords) {
+        super(context, numberOfWords);
+    }
+
+    @Override
+    public void execute() {
+        super.execute(mInputData);
+    }
+
+    @Override
+    public String tag() {
+        return TAG;
+    }
+
+    @Override
+    protected List<WordBundle> doInBackground(Integer... numberOfWords) {
+        // we expect exactly one number - input word
+        if (numberOfWords.length != 1) {
+            return null;
+        }
+        Integer num = numberOfWords[0];
+        DbHandler dbHandler = DbHandler.Factory.createLocalizedHelper(mContext, DbHandler.DB_USER_DICT);
+        if (dbHandler == null) {
+            Log.e(Constants.LOG_TAG, "db handler is suddenly null while getting random words from user dict");
+            return null;
+        }
+        return dbHandler.queryRandomWords(num);
+    }
+}
--- app/src/main/java/com/learnit/learnit/fragments/LearnWordsCardFragment.java
+++ app/src/main/java/com/learnit/learnit/fragments/LearnWordsCardFragment.java
@@ -14,28 +14,40 @@ package com.learnit.learnit.fragments;/*
  * limitations under the License.
  */
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.view.ViewCompat;
-import android.support.v7.widget.CardView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.learnit.learnit.R;
+import com.learnit.learnit.async_tasks.GetRandomUserWordsTask;
+import com.learnit.learnit.interfaces.IAsyncTaskResultClient;
+import com.learnit.learnit.interfaces.IRefreshable;
+import com.learnit.learnit.types.WordBundle;
+import com.learnit.learnit.utils.Constants;
+
+import java.util.List;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
-public class LearnWordsCardFragment extends Fragment {
+public class LearnWordsCardFragment
+        extends Fragment
+        implements IAsyncTaskResultClient, IRefreshable {
 
     private static final String ARG_POSITION = "position";
 
     @Bind(R.id.query_word)
-    TextView mTextView;
+    TextView mQueryWord;
 
-    private int position;
+
+    private TaskSchedulerFragment mTaskScheduler;
 
     public static LearnWordsCardFragment newInstance(int position) {
         LearnWordsCardFragment f = new LearnWordsCardFragment();
@@ -46,17 +58,80 @@ public class LearnWordsCardFragment extends Fragment {
     }
 
     @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        Log.d(Constants.LOG_TAG, "fragment is attached");
+        initTaskScheduler();
+    }
+
+    private void initTaskScheduler() {
+        FragmentManager fragmentManager = getFragmentManager();
+        mTaskScheduler = (TaskSchedulerFragment)
+                fragmentManager.findFragmentByTag(TaskSchedulerFragment.TAG);
+        if (mTaskScheduler == null) {
+            mTaskScheduler = new TaskSchedulerFragment();
+            fragmentManager.beginTransaction()
+                    .add(mTaskScheduler, TaskSchedulerFragment.TAG)
+                    .commit();
+        }
+    }
+
+
+    @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        position = getArguments().getInt(ARG_POSITION);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_learn_words, container, false);
         ButterKnife.bind(this, rootView);
-        ViewCompat.setElevation(rootView,50);
-        mTextView.setText("CARD "+position);
+        ViewCompat.setElevation(rootView, 50);
         return rootView;
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        updateWordsAsync();
+    }
+
+    private void updateWordsAsync() {
+        int numOfWords = 4;
+        mTaskScheduler.newTaskForClient(
+                new GetRandomUserWordsTask(this.getContext(), numOfWords), this);
+    }
+
+    @Override
+    public String tag() {
+        return "learn_words_fragment";
+    }
+
+    @Override
+    public void onPreExecute() {
+
+    }
+
+    @Override
+    public void onProgressUpdate(Float progress) {
+
+    }
+
+    @Override @SuppressWarnings("unchecked")
+    public <OutType> void onFinish(OutType result) {
+        if (result instanceof List) {
+            List<WordBundle> words = (List<WordBundle>) result;
+            mQueryWord.setText(words.get(0).word());
+        }
+    }
+
+    @Override
+    public void onCancelled() {
+
+    }
+
+    @Override
+    public void refresh() {
+        updateWordsAsync();
     }
 }
