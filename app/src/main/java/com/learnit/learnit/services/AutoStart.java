3ad48db6737939710286f6ff18b9beb7aa3ae0d3
Igor Bogoslavskyi <igor.bogoslavskyi@gmail.com>
2016-02-15 23:42:49 +0100

    added an autostart timer

--- app/src/main/java/com/learnit/learnit/services/AutoStart.java
+++ app/src/main/java/com/learnit/learnit/services/AutoStart.java
@@ -7,12 +7,14 @@ import android.util.Log;
 
 import com.learnit.learnit.R;
 import com.learnit.learnit.utils.Constants;
+import com.learnit.learnit.utils.Utils;
 import com.pixplicity.easyprefs.library.Prefs;
 
 public class AutoStart extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
-        if (Prefs.getBoolean(context.getString(R.string.key_pref_notifications_active), false))
-            Log.d(Constants.LOG_TAG, "need to turn on the notifications");
+        if (Prefs.getBoolean(context.getString(R.string.key_pref_notifications_active), false)) {
+            Utils.startRepeatingTimer(context);
+        }
     }
 }
